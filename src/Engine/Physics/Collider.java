7ed688d25098867e9c3730bdc09ad83ab2ee6623
KyvenT <kyventran@gmail.com>
2025-03-13 23:56:16 -0400

    moved DetectCollisions into Collider

--- src/Engine/Physics/Collider.java
+++ src/Engine/Physics/Collider.java
@@ -1,5 +1,7 @@
 package Engine.Physics;
 
+import Engine.Entities.Entity;
+
 public class Collider {
 
     public float radius;
@@ -10,5 +12,18 @@ public class Collider {
         enabled = false;
     }
 
+    public static Collision DetectCollision(Entity A, Entity B) {
+        Collision collision = new Collision(A, B);
+        float a_x = A.transform.position.x;
+        float a_y = A.transform.position.y;
 
+        float b_x = B.transform.position.x;
+        float b_y = B.transform.position.y;
+
+        double distance = Math.sqrt((b_x - a_x)*(b_x - a_x) + (b_y - a_y)*(b_y - a_y));
+        if (distance <= A.collider.radius + B.collider.radius) {
+            collision.hit = true;
+        }
+        return collision;
+    }
 }
