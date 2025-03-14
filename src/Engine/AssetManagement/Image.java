e0da90936aa1c9fcd4ef3f6d510609c5108278b2
Ryan Lambe <ryanlambe@hotmail.com>
2025-03-13 22:15:07 -0400

    added texture loading

--- src/Engine/AssetManagement/Image.java
+++ src/Engine/AssetManagement/Image.java
@@ -1,10 +1,15 @@
 package Engine.AssetManagement;
 
+import Engine.Graphics.Graphics;
+
 public class Image {
+
     public String filePath;
+    private int textureID;
 
     public Image(String filePath) {
         this.filePath = filePath;
+        textureID = Graphics.LoadTexture(filePath);
     }
 
     public String getFilePath() {
--- src/Engine/Graphics/Graphics.java
+++ src/Engine/Graphics/Graphics.java
@@ -3,10 +3,15 @@ package Engine.Graphics;
 import Engine.Application;
 import Engine.Entities.Entity;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.stb.STBImage;
 
 public class Graphics {
 
@@ -42,6 +47,36 @@ public class Graphics {
 
     public static void UpdateFramebufferSize(int width, int height) {
         GL11.glViewport(0, 0, width, height);
+    }
+
+    public static int LoadTexture(String filePath) {
+        int id = GL11.glGenTextures();
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer comp = BufferUtils.createIntBuffer(1);
+
+        ByteBuffer data = STBImage.stbi_load("./Assets/" + filePath, width, height, comp, 4);
+
+        if (data != null) {
+            GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width.get(), height.get(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data);
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            STBImage.stbi_image_free(data);
+            return id;
+        }
+
+        System.out.println("Failed to load texture: " + filePath + ".\n");
+        return 0;
+    }
+
+    public static void BindTexture(int textureID){
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
     }
 
 }
