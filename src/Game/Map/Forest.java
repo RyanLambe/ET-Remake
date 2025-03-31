e228519020048669a805fc3bb62ffa0978f517cb
Ryan Lambe <ryanlambe@hotmail.com>
2025-03-30 22:24:01 -0400

    added tutorial text

--- src/Game/Map/Forest.java
+++ src/Game/Map/Forest.java
@@ -1,11 +1,21 @@
 package Game.Map;
 
 import Engine.Application;
+import Engine.Entities.TextEntity;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 public class Forest extends Zone {
 
+    TextEntity tutorial;
+
     public Forest() {
         super("Forest", "ForestBG.png");
+        tutorial = Application.CreateEntity(new TextEntity());
+        tutorial.GetTextRenderer().SetText("Press W, A, S, D, or Arrow Keys to Move");
+        tutorial.GetTextRenderer().SetColor(new Vector4f(1, 1, 1, 1));
+        tutorial.transform.position = new Vector3f(-40, 25, -.1f);
+        tutorial.transform.Scale(0.75f);
     }
 
     @Override
@@ -18,5 +28,6 @@ public class Forest extends Zone {
     public void removeEntities() {
         // Example: Remove zone-specific objects when leaving
         System.out.println("Forest zone entities removed.");
+        tutorial.Destroy();
     }
 }
